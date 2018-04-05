16fc7a4be71cd51becb9b5d19b0eb2e3f106e1a3
ateller <tony.tel@yandex.ru>
2018-04-05 14:24:43 +0300

    Probably can read resolution

--- dream-filter.c
+++ dream-filter.c
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 //Я не настолько хорошо знаю английский,
 //чтобы писать на нем комментарии и не
@@ -22,26 +25,70 @@
 #endif
 //Макрос для обработки ошибок
 
-int isfile(char *path)
+#ifndef IS_BAD
+#define IS_BAD (condition)\
+	do { \
+		if (condition) { \
+			printf("Error: corrupted file\n");\
+			exit(EXIT_FAILURE);\
+		}\
+	} while (0)
+#endif
+//Макрос, чтобы писать, что файл плохой
+
+void filter_p6(int input)
 {
-	struct stat temp;
-	
-	if (stat(path, &temp) == -1)
-		return 0;
-	if (!S_ISREG(temp.st_mode))
-		return 0;	//не файл
-	return 1;
+	char buf[6], check;
+	int x, y, i;
+	check = read(input, buf, 1);
+	IF_ERR(check, -1, "Read error", exit(errno););
+	IS_BAD(buf[0] != '\n' || !check);
+	for(i = 0; i < 6; i++)
+	{
+		check = read(input, buf + i, 1);
+		IF_ERR(check, -1, "Read error", exit(errno););
+		IS_BAD(!check);
+		if(buf == ' ') 
+			break;
+		IS_BAD(!isdigit(buf[i]));
+	}
+	IS_BAD(!isdigit(i == 6);
+	buf[i] = 0;
+	x = atoi(buf);
+	IS_BAD(x > 65535);
+	for(i = 0; i < 6; i++)
+	{
+		check = read(input, buf + i, 1);
+		IF_ERR(check, -1, "Read error", exit(errno););
+		IS_BAD(!check);
+		if(buf == '\n') 
+			break;
+		IS_BAD(!isdigit(buf[i]));
+	}
+	IS_BAD(!isdigit(i == 6);
+	buf[i] = 0;
+	y = atoi(buf);
+	IS_BAD(y > 65535);
 }
 
 int main(int argc, char *argv[])
 {
+	int input;
+	char buf[2];
 	if (argc == 1) {
 		printf("Error: there is no arguments\n");
 		exit(EXIT_FAILURE);
 	}
-	if (!isfile(argv[1])) {
-		printf("Error, %s is not file\n", argv[1]);
-		exit(EXIT_FAILURE);
+	input = open(argv[1], O_RDONLY);
+	IF_ERR(input, -1, argv[1], exit(errno););
+	IF_ERR(read(input, buf, 2), -1, "Read error", exit(errno););
+	IS_BAD(buf[0] != 'P' || buf[1] < '1' || buf[1] > '6');
+	switch(buf[1]) {
+	case '6':
+		filter_p6(input);
+		break;
+	default:
+		printf("Sorry, this kind of ppm is not supported yet\n");
+		exit(EXIT_SUCCESS);
 	}
-	
 }
